ba3b8829b0a3eccd521a445c936da3fd2490259a
VOIRET <gautier.voiret@etudiant.univ-reims.fr>
2023-11-13 21:54:15 +0100

    Ajout fonction show dans EvenementController + bundle expression-language

--- src/Controller/EvenementController.php
+++ src/Controller/EvenementController.php
@@ -2,18 +2,20 @@
 
 namespace App\Controller;
 
+use App\Entity\Evenement;
+use Symfony\Bridge\Doctrine\Attribute\MapEntity;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class EvenementController extends AbstractController
 {
-    #[Route('/evenement', name: 'app_evenement')]
-    public function index(): JsonResponse
+    #[Route('/evenement/{id}', name: 'app_evenement_id', requirements: ['eventId' => '\d+'])]
+    public function show(#[MapEntity(expr: 'repository.findWithCategory(id)')]
+        ?Evenement $evenement): Response
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/EvenementController.php',
-        ]);
+
+        return $this->render('evenement/show.html.twig', [
+            'evenement' => $evenement]);
     }
 }
