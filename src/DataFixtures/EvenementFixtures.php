df0bb3801b561d46c1a7cb3533e7e8a6cfa9d4ba
VOIRET <gautier.voiret@etudiant.univ-reims.fr>
2023-11-13 21:40:21 +0100

    Fonction load ecrite (pas encore teste)

--- src/DataFixtures/EvenementFixtures.php
+++ src/DataFixtures/EvenementFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Factory\EvenementFactory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -9,9 +10,10 @@ class EvenementFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $tab = json_decode(file_get_contents(__DIR__.'/data/Evenement.json'), true);
+        foreach ($tab as $element) {
+            EvenementFactory::createOne(['nomEvent' => $element['nomEvent']]);
+        }
 
-        $manager->flush();
     }
 }
