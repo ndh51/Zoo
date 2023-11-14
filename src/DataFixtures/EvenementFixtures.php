6b3155b31e619e6a24e0fe875123b6ea2ba07f75
VOIRET <gautier.voiret@etudiant.univ-reims.fr>
2023-11-14 20:47:53 +0100

    Fix création factice de nbPlaceMaxEvent dans l'entite Evenement

--- src/DataFixtures/EvenementFixtures.php
+++ src/DataFixtures/EvenementFixtures.php
@@ -8,13 +8,17 @@ use Doctrine\Persistence\ObjectManager;
 
 class EvenementFixtures extends Fixture
 {
+    /**
+     * @throws \Exception
+     */
     public function load(ObjectManager $manager): void
     {
         $tab = json_decode(file_get_contents(__DIR__.'/data/Evenement.json'), true);
         $evenements = $tab['evenements'];
         foreach ($evenements as $evenement) {
             EvenementFactory::createOne(['nomEvent' => $evenement['nom'],
-                'descEvent' => $evenement['description']]);
+                'descEvent' => $evenement['description'],
+                'nbPlaceMaxEvent' => random_int(15, 100)]);
         }
     }
 }
