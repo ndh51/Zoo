9d1870399636559ce22562eaa6ed020d7e3ae24b
VOIRET <gautier.voiret@etudiant.univ-reims.fr>
2023-11-15 18:37:35 +0100

    Ajout relation entre Evenement & Enclos

--- src/Entity/Enclos.php
+++ src/Entity/Enclos.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\EnclosRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: EnclosRepository::class)]
@@ -15,6 +17,14 @@ class Enclos
 
     #[ORM\Column(length: 30)]
     private ?string $nomEnclos = null;
+
+    #[ORM\OneToMany(mappedBy: 'idEnclos', targetEntity: Evenement::class)]
+    private Collection $evenements;
+
+    public function __construct()
+    {
+        $this->evenements = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -29,6 +39,36 @@ class Enclos
     public function setNomEnclos(string $nomEnclos): static
     {
         $this->nomEnclos = $nomEnclos;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Evenement>
+     */
+    public function getEvenements(): Collection
+    {
+        return $this->evenements;
+    }
+
+    public function addEvenement(Evenement $evenement): static
+    {
+        if (!$this->evenements->contains($evenement)) {
+            $this->evenements->add($evenement);
+            $evenement->setIdEnclos($this);
+        }
+
+        return $this;
+    }
+
+    public function removeEvenement(Evenement $evenement): static
+    {
+        if ($this->evenements->removeElement($evenement)) {
+            // set the owning side to null (unless already changed)
+            if ($evenement->getIdEnclos() === $this) {
+                $evenement->setIdEnclos(null);
+            }
+        }
 
         return $this;
     }
