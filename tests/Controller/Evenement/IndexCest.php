ad49f859f3ae103594fa92b9379fd91a4203436d
Léo Bourgasser <leo.bourgasser@etudiant.univ-reims.fr>
2023-11-16 18:27:22 +0100

    Ajout du test On Click On A Evenement

--- tests/Controller/Evenement/IndexCest.php
+++ tests/Controller/Evenement/IndexCest.php
@@ -14,4 +14,14 @@ class IndexCest
         $I->seeResponseCodeIs(200);
         $I->seeNumberOfElements('#evenements > a', 5);
     }
+
+    public function testOnClickOnAEvenement(ControllerTester $I): void
+    {
+        EvenementFactory::createOne(['nomEvent' => 'Aaaaaaaaaa', 'descEvent' => 'Un spectacle de mouettes plutôt sympatique', 'nbPlaceMaxEvent' => 47]);
+        EvenementFactory::createMany(5);
+        $I->amOnPage('/evenement');
+        $I->seeResponseCodeIs(200);
+        $I->click('#evenements > a');
+        $I->seeCurrentRouteIs('app_evenement_id', ['id' => 1]);
+    }
 }
