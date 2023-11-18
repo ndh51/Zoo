507641326571b60a29f76116fa2525a4b6f5a2ff
Léo Bourgasser <leo.bourgasser@etudiant.univ-reims.fr>
2023-11-18 23:53:25 +0100

    Ajout du test On Click On An Animal

---
+++ tests/Controller/Animal/IndexCest.php
@@ -0,0 +1,28 @@
+<?php
+
+
+namespace App\Tests\Controller\Animal;
+
+use App\Factory\AnimalFactory;
+use App\Tests\Support\ControllerTester;
+
+class IndexCest
+{
+    public function testOnListOfAnimal(ControllerTester $I): void
+    {
+        AnimalFactory::createMany(5);
+        $I->amOnPage('/animal');
+        $I->seeResponseCodeIs(200);
+        $I->seeNumberOfElements('.list-group > a', 5);
+    }
+
+    public function testOnClickOnAAnimal(ControllerTester $I): void
+    {
+        AnimalFactory::createOne(['nomAnimal' => 'Aaaaaaaaaa', 'descAnimal' => 'Un animal rare au nom facilement prononcable']);
+        AnimalFactory::createMany(5);
+        $I->amOnPage('/animal');
+        $I->seeResponseCodeIs(200);
+        $I->click('.list-group > a');
+        $I->seeCurrentRouteIs('app_animal_show', ['id' => 1]);
+    }
+}
