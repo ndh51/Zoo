1ac3c9765ee05229707469e238c767b9333bbbc7
Gautier Voiret <gautier.voiret@etudiant.univ-reims.fr>
2023-11-18 13:25:32 +0100

    Initialisation tests Controller Home

---
+++ tests/Controller/Home/IndexCest.php
@@ -0,0 +1,18 @@
+<?php
+
+
+namespace App\Tests\Controller\Home;
+
+use App\Tests\Support\ControllerTester;
+
+class IndexCest
+{
+    public function _before(ControllerTester $I)
+    {
+    }
+
+    // tests
+    public function tryToTest(ControllerTester $I)
+    {
+    }
+}
