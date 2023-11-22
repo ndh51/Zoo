cfe7beaa7ae49b3e78641a40d146a3533f7e6f97
VOIRET <gautier.voiret@etudiant.univ-reims.fr>
2023-11-22 20:07:13 +0100

    Ajout test CLickOnAEvenement de la page / & correction Factory (pour les tests)

--- tests/Controller/Home/IndexCest.php
+++ tests/Controller/Home/IndexCest.php
@@ -5,6 +5,7 @@ namespace App\Tests\Controller\Home;
 
 use App\Entity\Animal;
 use App\Factory\AnimalFactory;
+use App\Factory\EnclosFactory;
 use App\Factory\EvenementFactory;
 use App\Tests\Support\ControllerTester;
 
@@ -18,6 +19,18 @@ class IndexCest
         $I->seeResponseCodeIs(200);
         $I->seeNumberOfElements('#evenements > a', 3);
         $I->seeNumberOfElements('#animals > a', 9);
+    }
+
+    public function testOnClickOnAEvenement(ControllerTester $I): void
+    {
+        EvenementFactory::createOne(['nbPlaceMaxEvent' => 50000]);
+        EvenementFactory::createMany(5);
+        AnimalFactory::createMany(9);
+        $I->amOnPage('/');
+        $I->seeResponseCodeIs(200);
+        $I->click('#evenements > a');
+        $I->seeCurrentRouteIs('app_evenement_id', ['id' => 1]);
+        $I->seeResponseCodeIs(200);
     }
 }
 
