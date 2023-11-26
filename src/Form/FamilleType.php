375c4af4fc60b883d5a6018c6137c6c7be87fd17
Arthur Schneider <arthur.schneider@etudiant.univ-reims.fr>
2023-11-26 10:41:32 +0100

    creation du form famille

---
+++ src/Form/FamilleType.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Famille;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class FamilleType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('nomFamille')
+            ->add('descFamille')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Famille::class,
+        ]);
+    }
+}
