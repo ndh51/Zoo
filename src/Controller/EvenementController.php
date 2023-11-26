631f9f365e721b40e915b629d8b396b4d462b85e
VOIRET <gautier.voiret@etudiant.univ-reims.fr>
2023-11-26 22:44:20 +0100

    Ajout methode update dans le controller

--- src/Controller/EvenementController.php
+++ src/Controller/EvenementController.php
@@ -3,8 +3,10 @@
 namespace App\Controller;
 
 use App\Entity\Evenement;
+use App\Form\EvenementType;
 use App\Repository\EvenementRepository;
 use Doctrine\DBAL\Exception;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bridge\Doctrine\Attribute\MapEntity;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -35,4 +37,23 @@ class EvenementController extends AbstractController
         return $this->render('evenement/show.html.twig', [
             'evenement' => $evenement]);
     }
+
+    #[Route('/evenement/{id}/update', name: 'app_evenement_update', requirements: ['id' => '\d+'])]
+    public function update(Evenement $evenement, Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(EvenementType::class, $evenement);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_evenement_id', ['id' => $evenement->getId()]);
+        }
+
+        return $this->render('evenement/update.html.twig', [
+            'evenement' => $evenement,
+            'form' => $form->createView(),
+            'sumbmit_label' => 'Modifier']);
+    }
+
 }
