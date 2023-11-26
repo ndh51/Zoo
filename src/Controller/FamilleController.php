c0bfa9385ec42aae4f8920d8e729ab1041ec7495
Arthur Schneider <arthur.schneider@etudiant.univ-reims.fr>
2023-11-26 10:11:57 +0100

    ajout des route CRUD, affichage  des familles

--- src/Controller/FamilleController.php
+++ src/Controller/FamilleController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\FamilleRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +10,29 @@ use Symfony\Component\Routing\Annotation\Route;
 class FamilleController extends AbstractController
 {
     #[Route('/famille', name: 'app_famille')]
-    public function index(): Response
+    public function index(FamilleRepository $repository): Response
     {
+        $familles = $repository->findBy([]);
         return $this->render('famille/index.html.twig', [
-            'controller_name' => 'FamilleController',
+            'familles' => $familles,
         ]);
+    }
+
+    #[Route('/famille/create', name: 'app_famille_create')]
+    public function create(): Response
+    {
+
+    }
+
+    #[Route('/famille/id<\d+>}/update', name: 'app_famille_id_update')]
+    public function update(): Response
+    {
+
+    }
+
+    #[Route('/famille/{id<\d+>}/delete', name: 'app_famille_id_delete')]
+    public function delete(): Response
+    {
+
     }
 }
