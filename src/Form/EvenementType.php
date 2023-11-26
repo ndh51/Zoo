44927fadf524856a0e3b32687f0858961b304554
VOIRET <gautier.voiret@etudiant.univ-reims.fr>
2023-11-26 22:26:12 +0100

    Ajout form Evenement

---
+++ src/Form/EvenementType.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Evenement;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class EvenementType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('nomEvent')
+            ->add('descEvent')
+            ->add('nbPlaceMaxEvent')
+            ->add('idEnclos')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Evenement::class,
+        ]);
+    }
+}
