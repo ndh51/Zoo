c96fdfd6ade2b4b51e80e8751c72dcab031320de
hale0010 <nadhir.hales@etudiant.univ-reims.fr>
2023-12-04 02:05:59 +0100

    ajout fonction suppression

--- src/Controller/AnimalController.php
+++ src/Controller/AnimalController.php
@@ -8,6 +8,7 @@ use App\Repository\AnimalRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bridge\Doctrine\Attribute\MapEntity;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -50,7 +51,6 @@ class AnimalController extends AbstractController
 
         return $this->render('animal/create.html.twig',
             ['form' => $form->createView()]);
-
     }
 
     #[Route('/animal/{id}/update', name: 'app_animal_id_update', requirements: ['animalId' => '\d+'])]
@@ -74,5 +74,28 @@ class AnimalController extends AbstractController
     #[Route('/animal/{id}/delete', name: 'app_animal_id_delete', requirements: ['animalId' => '\d+'])]
     public function delete(Request $request, ?Animal $animal, EntityManagerInterface $entityManager)
     {
+        $form = $this->createFormBuilder()
+            ->add('Supprimer', SubmitType::class, ['label' => 'Supprimer'])
+            ->add('Annuler', SubmitType::class, ['label' => 'Annuler'])
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($request->isMethod('POST') && $form->isSubmitted() && $form->isValid()) {
+            $clickedButton = $form->getClickedButton();
+
+            if ($clickedButton && 'Supprimer' === $clickedButton->getName()) {
+                $entityManager->remove($animal);
+                $entityManager->flush();
+
+                return $this->redirectToRoute('app_animal', status: 303);
+            } else {
+                return $this->redirectToRoute('app_animal_id', ['id' => $animal->getId()], status: 303);
+            }
+        }
+
+        return $this->render('animal/delete.html.twig', [
+            'animal' => $animal,
+            'form' => $form]);
     }
 }
