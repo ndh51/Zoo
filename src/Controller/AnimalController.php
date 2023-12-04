44a8a97aa362047a2f018cdf39c1da50198a66cc
hale0010 <nadhir.hales@etudiant.univ-reims.fr>
2023-12-04 01:17:18 +0100

    ajout fonction create (pb dans l'execution)

--- src/Controller/AnimalController.php
+++ src/Controller/AnimalController.php
@@ -5,8 +5,10 @@ namespace App\Controller;
 use App\Entity\Animal;
 use App\Form\AnimalType;
 use App\Repository\AnimalRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bridge\Doctrine\Attribute\MapEntity;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -33,14 +35,35 @@ class AnimalController extends AbstractController
     }
 
     #[Route('/animal/create', name: 'app_animal_create', requirements: ['animalId' => '\d+'])]
-    public function create()
+    public function create(Request $request, EntityManagerInterface $entityManager)
     {
+        $animal = new Animal();
+        $form = $this->createForm(AnimalType::class, $animal);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($animal);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_animal_id', ['id' => $animal->getId()]);
+        }
+
+        return $this->render('animal/create.html.twig',
+            ['form' => $form->createView()]);
+
     }
 
     #[Route('/animal/{id}/update', name: 'app_animal_id_update', requirements: ['animalId' => '\d+'])]
-    public function update(?Animal $animal)
+    public function update(Request $request, ?Animal $animal, EntityManagerInterface $entityManager)
     {
         $form = $this->createForm(AnimalType::class, $animal);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_animal_id', ['id' => $animal->getId()]);
+        }
 
         return $form = $this->render('animal/update.html.twig', [
             'animal' => $animal,
@@ -49,7 +72,7 @@ class AnimalController extends AbstractController
     }
 
     #[Route('/animal/{id}/delete', name: 'app_animal_id_delete', requirements: ['animalId' => '\d+'])]
-    public function delete(?Animal $animal)
+    public function delete(Request $request, ?Animal $animal, EntityManagerInterface $entityManager)
     {
     }
 }
