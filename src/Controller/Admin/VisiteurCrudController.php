3a6368bc66d0f8cc5f5f4932cd1696f34125b752
Voiret Gautier <gautier.voiret@etudiant.univ-reims.fr>
2023-12-13 15:44:55 +0100

    Ajout de methode pour changer le mot de passe d'un Visiteur

--- src/Controller/Admin/VisiteurCrudController.php
+++ src/Controller/Admin/VisiteurCrudController.php
@@ -3,13 +3,23 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Visiteur;
+use Doctrine\ORM\EntityManagerInterface;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class VisiteurCrudController extends AbstractCrudController
 {
+
+    private UserPasswordHasherInterface $userPasswordHasher;
+
+    public function __construct(UserPasswordHasherInterface $userPasswordHasher)
+    {
+        $this->userPasswordHasher = $userPasswordHasher;
+    }
+
     public static function getEntityFqcn(): string
     {
         return Visiteur::class;
@@ -25,4 +35,24 @@ class VisiteurCrudController extends AbstractCrudController
         ];
     }
     */
+    public function updateEntity(EntityManagerInterface $entityManager, $entityInstance): void
+    {
+        $this->setVisiteurPassword($entityInstance, $entityManager);
+    }
+
+    public function persistEntity(EntityManagerInterface $entityManager, $entityInstance): void
+    {
+        $this->setVisiteurPassword($entityInstance, $entityManager);
+        parent::persistEntity($entityManager, $entityInstance);
+    }
+
+    public function setVisiteurPassword($entityInstance, EntityManagerInterface $entityManager): void
+    {
+        $pwd = $this->getContext()->getRequest()->get('Visiteur')['password'];
+        if ($pwd) {
+            $entityInstance->setPassword($this->userPasswordHasher->hashPassword($entityInstance, $pwd));
+        }
+
+        parent::updateEntity($entityManager, $entityInstance);
+    }
 }
