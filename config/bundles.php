5f5e86156c4ddf997495219f761659702ee9c85c
hale0010 <nadhir.hales@etudiant.univ-reims.fr>
2023-12-14 15:34:20 +0100

    suppression webprofiler et stopwatch

--- config/bundles.php
+++ config/bundles.php
@@ -8,5 +8,4 @@ return [
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Zenstruck\Foundry\ZenstruckFoundryBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
-    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
 ];
