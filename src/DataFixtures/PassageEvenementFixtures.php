53dcd075a6a3c8874a2ae6f7e77fceb2e549c038
Arthur Schneider <arthur.schneider@etudiant.univ-reims.fr>
2023-12-17 10:04:45 +0100

    ajout de PassageEvenement.json, generation des passage avec ce json

--- src/DataFixtures/PassageEvenementFixtures.php
+++ src/DataFixtures/PassageEvenementFixtures.php
@@ -2,20 +2,39 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Evenement;
 use App\Factory\PassageEvenementFactory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
+use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\Persistence\ObjectManager;
 
 class PassageEvenementFixtures extends Fixture implements OrderedFixtureInterface
 {
+    private EntityManagerInterface $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
     public function load(ObjectManager $manager): void
     {
         // $product = new Product();
         // $manager->persist($product);
 
         // $manager->flush();
-        PassageEvenementFactory::createMany(50);
+        $tab = json_decode(file_get_contents(__DIR__.'/data/PassageEvenement.json'), true);
+        $passages = $tab['passageEvenement'];
+
+        $evenement = $this->entityManager->getRepository(Evenement::class);
+        foreach ($passages as $passage) {
+            PassageEvenementFactory::createOne([
+                'hDebEvenement' => $passage['hDebEvenement'],
+                'hFinEvenement' => $passage['hFinEvenement'],
+                'idEvenement' => $evenement->findOneBy(['nomEvent' => $passage['idEvenement']]),
+            ]);
+        }
     }
 
     public function getOrder(): int
