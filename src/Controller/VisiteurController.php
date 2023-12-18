b362a0ec0e5d117a085471f166be49288bc78e06
VOIRET <gautier.voiret@etudiant.univ-reims.fr>
2023-12-18 18:35:14 +0100

    Ajout page show dans Controller Visiteur

--- src/Controller/VisiteurController.php
+++ src/Controller/VisiteurController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Visiteur;
+use Symfony\Bridge\Doctrine\Attribute\MapEntity;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -14,5 +16,16 @@ class VisiteurController extends AbstractController
         return $this->render('visiteur/index.html.twig', [
             'controller_name' => 'VisiteurController',
         ]);
+    }
+
+    #[Route('/visiteur/{id}', name: 'app_visiteur_id', requirements: ['id' => '\d+'])]
+    public function show(#[MapEntity(expr: 'repository.findWithId(id)')] ?Visiteur $visiteur): Response
+    {
+        if (is_null($visiteur)) {
+            return $this->redirectToRoute('app_visiteur', status: 303);
+        }
+
+        return $this->render('visiteur/show.html.twig', [
+            'visiteur' => $visiteur]);
     }
 }
