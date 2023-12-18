73e0783e83130f9079a7f00dbcb5efd619ae4195
Louis Gardin <louis.gardin@etudiant.univ-reims.fr>
2023-12-18 19:51:06 +0100

    Récupération des évènements réservés par le visiteur

--- src/Controller/VisiteurController.php
+++ src/Controller/VisiteurController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Visiteur;
+use App\Repository\TicketRepository;
 use Symfony\Bridge\Doctrine\Attribute\MapEntity;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -10,22 +11,15 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class VisiteurController extends AbstractController
 {
-    #[Route('/visiteur', name: 'app_visiteur')]
-    public function index(): Response
-    {
-        return $this->render('visiteur/index.html.twig', [
-            'controller_name' => 'VisiteurController',
-        ]);
-    }
-
     #[Route('/visiteur/{id}', name: 'app_visiteur_id', requirements: ['id' => '\d+'])]
-    public function show(#[MapEntity(expr: 'repository.findWithId(id)')] ?Visiteur $visiteur): Response
+    public function show(#[MapEntity(expr: 'repository.findWithId(id)')] ?Visiteur $visiteur, TicketRepository $ticketRepository): Response
     {
+        $tickets = $ticketRepository->findEventsByVisiteur($visiteur->getId());
         if (is_null($visiteur)) {
-            return $this->redirectToRoute('app_visiteur', status: 303);
+            return $this->redirectToRoute('app_home', status: 303);
         }
 
         return $this->render('visiteur/show.html.twig', [
-            'visiteur' => $visiteur]);
+            'visiteur' => $visiteur, 'tickets' => $tickets]);
     }
 }
