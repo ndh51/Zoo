9a82c40d0e7880ae38d941f8ba24d342911fee6a
VOIRET <gautier.voiret@etudiant.univ-reims.fr>
2023-12-18 20:17:19 +0100

    Ajout sécurite dans la page d'affichage d'un Visiteur

--- src/Controller/VisiteurController.php
+++ src/Controller/VisiteurController.php
@@ -14,12 +14,26 @@ class VisiteurController extends AbstractController
     #[Route('/visiteur/{id}', name: 'app_visiteur_id', requirements: ['id' => '\d+'])]
     public function show(#[MapEntity(expr: 'repository.findWithId(id)')] ?Visiteur $visiteur, TicketRepository $ticketRepository): Response
     {
-        $tickets = $ticketRepository->findEventsByVisiteur($visiteur->getId());
+        $currentUser = $this->getUser();
+
         if (is_null($visiteur)) {
             return $this->redirectToRoute('app_home', status: 303);
         }
 
-        return $this->render('visiteur/show.html.twig', [
-            'visiteur' => $visiteur, 'tickets' => $tickets]);
+        // Si l'utilisateur n'est pas connecté, redirigez-le vers la page de connexion
+        if (!$currentUser) {
+            return $this->redirectToRoute('app_login');
+        }
+
+        $userIdFromRoute = $visiteur->getId();
+
+        if (in_array('ROLE_ADMIN', $currentUser->getRoles(), true) || $currentUser->getId() == $userIdFromRoute) {
+            $tickets = $ticketRepository->findEventsByVisiteur($visiteur->getId());
+
+            return $this->render('visiteur/show.html.twig', [
+                'visiteur' => $visiteur, 'tickets' => $tickets]);
+        }
+
+        return $this->redirectToRoute('app_home', status: 303);
     }
 }
