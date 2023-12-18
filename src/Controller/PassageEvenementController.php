f7c289de07518016547d112ec2e48ab47f2cc917
Arthur Schneider <arthur.schneider@etudiant.univ-reims.fr>
2023-12-18 19:12:00 +0100

    ajout de show

--- src/Controller/PassageEvenementController.php
+++ src/Controller/PassageEvenementController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\PassageEvenement;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -15,4 +16,13 @@ class PassageEvenementController extends AbstractController
             'controller_name' => 'PassageEvenementController',
         ]);
     }
+
+    #[Route('/passage/evenement/{id<\d+>}', name: 'app_passage_evenement_id')]
+    public function show(PassageEvenement $passageEvenement): Response
+    {
+        return $this->render('passageEvenement/show.html.twig', [
+            'passageEvenement' => $passageEvenement,
+        ]);
+    }
+
 }
