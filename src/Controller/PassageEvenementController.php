f5ad39777ba47df5447ad0fd2c82964e923aed69
Arthur Schneider <arthur.schneider@etudiant.univ-reims.fr>
2023-12-20 20:11:03 +0100

    ajout de la suppretion de passageEvenement

--- src/Controller/PassageEvenementController.php
+++ src/Controller/PassageEvenementController.php
@@ -6,6 +6,7 @@ use App\Entity\PassageEvenement;
 use App\Form\PassageEvenementType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -53,6 +54,34 @@ class PassageEvenementController extends AbstractController
         $form = $this->createForm(PassageEvenementType::class, $passageEvenement);
 
         return $this->render('passageEvenement/update.html.twig', [
+            'passageEvenement' => $passageEvenement,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/passage/evenement/{id<\d+>}/delete', name: 'app_passage_evenement_delete')]
+    public function delete(PassageEvenement $passageEvenement, Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createFormBuilder()
+            ->add('delete', SubmitType::class, ['label' => 'delete'])
+            ->add('cancel', SubmitType::class, ['label' => 'cancel'])
+            ->getForm();
+
+        $form->handleRequest($request);
+        if ($request->isMethod('POST') && $form->isSubmitted() && $form->isValid()) {
+            $clickedButton = $form->getClickedButton();
+
+            if ($clickedButton && 'delete' === $clickedButton->getName()) {
+                $idEvent = $passageEvenement->getEvenement()->getId();
+                $entityManager->remove($passageEvenement);
+                $entityManager->flush();
+                return $this->redirectToRoute('app_evenement_id', ['id' => $idEvent], status: 303);
+            } else {
+                return $this->redirectToRoute('app_passage_evenement_id', ['id' => $passageEvenement->getId()], status: 303);
+            }
+        }
+
+        return $this->render('passageEvenement/delete.html.twig', [
             'passageEvenement' => $passageEvenement,
             'form' => $form,
         ]);
