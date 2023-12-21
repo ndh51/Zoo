87a100a4c7e2be48e207d965d1c53f4f2400f4ab
VOIRET <gautier.voiret@etudiant.univ-reims.fr>
2023-12-21 13:19:10 +0100

    Modif du formulaire de register

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -6,18 +6,28 @@ use App\Entity\Visiteur;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Regex;
 
 class RegistrationFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
+            ->add('email', TextType::class, [
+                'label' => 'Email',
+                'constraints' => [
+                    new Regex([
+                        'pattern' => '/^[^@]+@[^@]+\.[a-zA-Z]{2,}$/',
+                        'message' => 'L\'adresse email doit être sous la forme "quelquechose@quelquechose.quelquechose".',
+                    ]),
+                ],
+            ])
             ->add('agreeTerms', CheckboxType::class, [
                                 'mapped' => false,
                 'constraints' => [
@@ -26,9 +36,55 @@ class RegistrationFormType extends AbstractType
                     ]),
                 ],
             ])
+            ->add('nomVisiteur', TextType::class, [
+                'label' => 'Nom',
+                'constraints' => [
+                    new Regex([
+                        'pattern' => '/^[A-Z][a-zA-Z]*$/',
+                        'message' => 'Le nom doit commencer par une majuscule.',
+                    ]),
+                ],
+            ])
+            ->add('prenomVisiteur', TextType::class, [
+                'label' => 'Prénom',
+                'constraints' => [
+                    new Regex([
+                        'pattern' => '/^[A-Z][a-zA-Z]*$/',
+                        'message' => 'Le prénom doit commencer par une majuscule.',
+                    ]),
+                ],
+            ])
+            ->add('villeVisiteur', TextType::class, [
+                'label' => 'Ville',
+                'constraints' => [
+                    new Regex([
+                        'pattern' => '/^[A-Z][a-zA-Z]*$/',
+                        'message' => 'La ville doit commencer par une majuscule.',
+                    ]),
+                ],
+            ])
+            ->add('adVisiteur', TextType::class, [
+                'label' => 'Adresse',
+            ])
+            ->add('cpVisiteur', TextType::class, [
+                'label' => 'Code postal',
+                'constraints' => [
+                    new Regex([
+                        'pattern' => '/^\d{5}$/',
+                        'message' => 'Le code postal doit contenir exactement cinq chiffres.',
+                    ]),
+                ],
+            ])
+            ->add('telVisiteur', TextType::class, [
+                'label' => 'Téléphone',
+                'constraints' => [
+                    new Regex([
+                        'pattern' => '/^\d{10}$/',
+                        'message' => 'Le numéro de téléphone doit contenir exactement dix chiffres.',
+                    ]),
+                ],
+            ])
             ->add('plainPassword', PasswordType::class, [
-                                // instead of being set onto the object directly,
-                // this is read and encoded in the controller
                 'mapped' => false,
                 'attr' => ['autocomplete' => 'new-password'],
                 'constraints' => [
@@ -38,7 +94,6 @@ class RegistrationFormType extends AbstractType
                     new Length([
                         'min' => 6,
                         'minMessage' => 'Your password should be at least {{ limit }} characters',
-                        // max length allowed by Symfony for security reasons
                         'max' => 4096,
                     ]),
                 ],
