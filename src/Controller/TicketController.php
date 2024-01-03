630ff670bd68aeb93b796b7e891861f900f128e8
Léo Bourgasser <leo.bourgasser@etudiant.univ-reims.fr>
2024-01-03 18:57:06 +0100

    Ajout de la date dans l'action du controleur

--- src/Controller/TicketController.php
+++ src/Controller/TicketController.php
@@ -7,13 +7,14 @@ use App\Entity\Visiteur;
 use App\Form\TicketType;
 use Symfony\Bridge\Doctrine\Attribute\MapEntity;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class TicketController extends AbstractController
 {
     #[Route('/ticket/{id}', name: 'app_ticket', requirements: ['id' => '\d+'])]
-    public function create(#[MapEntity(expr: 'repository.findWithId(id)')] ?Visiteur $visiteur): Response
+    public function create(#[MapEntity(expr: 'repository.findWithId(id)')] ?Visiteur $visiteur, Request $request): Response
     {
         $currentUser = $this->getUser();
 
@@ -29,9 +30,15 @@ class TicketController extends AbstractController
         $userIdFromRoute = $visiteur->getId();
 
         if ($currentUser->getId() == $userIdFromRoute) {
+            $date = $request->query->get('date', '');
+            if ('' == $date) {
+                return $this->redirectToRoute('app_visiteur_id', ['id' => $currentUser->getId()]);
+            }
+
             $ticket = new Ticket();
             $form = $this->createForm(TicketType::class, $ticket, [
                 'currentVisiteur' => $visiteur,
+                'date' => $date,
             ]);
 
             return $this->render('ticket/index.html.twig', [
