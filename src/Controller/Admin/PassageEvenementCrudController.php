c3b85e0642a5cfc178caabf7095e0ab1f0b177e5
Arthur Schneider <arthur.schneider@etudiant.univ-reims.fr>
2024-01-03 14:46:25 +0100

    generation et ajout du crud passageEvenement dans le dashboard

---
+++ src/Controller/Admin/PassageEvenementCrudController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\PassageEvenement;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+
+class PassageEvenementCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return PassageEvenement::class;
+    }
+
+    /*
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            IdField::new('id'),
+            TextField::new('title'),
+            TextEditorField::new('description'),
+        ];
+    }
+    */
+}
