37bfe1b0bcebb53794721abcb778b68c19b24a69
Arthur Schneider <arthur.schneider@etudiant.univ-reims.fr>
2024-01-03 15:01:58 +0100

    configuration du crud passageEvenement dans le dashboard

--- src/Controller/Admin/PassageEvenementCrudController.php
+++ src/Controller/Admin/PassageEvenementCrudController.php
@@ -3,9 +3,10 @@
 namespace App\Controller\Admin;
 
 use App\Entity\PassageEvenement;
+use Doctrine\ORM\EntityRepository;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
-use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
 class PassageEvenementCrudController extends AbstractCrudController
@@ -15,14 +16,22 @@ class PassageEvenementCrudController extends AbstractCrudController
         return PassageEvenement::class;
     }
 
-    /*
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            TextField::new('hDebEvenement'),
+            DateField::new('datePassage'),
+            AssociationField::new('evenement')
+                ->setFormTypeOptions([
+                    'choice_label' => 'nomEvent',
+                    'query_builder' => function (EntityRepository $entityRepository) {
+                        return $entityRepository->createQueryBuilder('e')
+                            ->orderBy('e.nomEvent', 'ASC');
+                    },
+                ])
+                ->formatValue(function ($value, $entity) {
+                    return $entity->getEvenement()->getNomEvent();
+                }),
         ];
     }
-    */
 }
