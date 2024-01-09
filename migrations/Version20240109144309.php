dffd849c0c0d438c52ba0de1c9fde3d8eca43447
Léo Bourgasser <leo.bourgasser@etudiant.univ-reims.fr>
2024-01-09 16:00:28 +0100

    Ajout des passages de la semaines sur la page de description du ticket

---
+++ migrations/Version20240109144309.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20240109144309 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE passage_evenement CHANGE nb_places_restantes nb_places_restantes INT DEFAULT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE passage_evenement CHANGE nb_places_restantes nb_places_restantes INT NOT NULL');
+    }
+}
